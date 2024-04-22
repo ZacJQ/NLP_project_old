b9da5d08920f9e0a560bb1c182b028e6908ce9a7
ZacJQ <141170384+ZacJQ@users.noreply.github.com>
2024-04-22 14:46:44 +0530

    Addition of Q_A and upadated scripts

--- q_a_creation.py
+++ q_a_creation.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+start_time = time.time()
 
 def google_translate(input_text:str, output_text:str)-> str:
     return output_text
@@ -38,8 +39,9 @@ xpath_button = "//button[@data-testid='send-button']"
 xpath_reply = ".//div[@class='markdown prose w-full break-words dark:prose-invert dark']/parent::*"
 xpath_scroll = "//div[@class='w-full text-token-text-primary']"
 num = 3
-csv_loc = "Dataset/Final_Dataset/Data_complete.csv"
-
+csv_loc = "Data_complete.csv"
+sucess = 0 
+errors = 0
 
 driver = webdriver.Firefox()
 driver.get(url)
@@ -47,16 +49,24 @@ data_df = pd.read_csv(csv_loc)
 question_df = data_df['Description']
 data = []
 
-for question_inp in question_df:
+for question_inp in question_df[675:-1]:
     try:
         xpath_reply_box = f"//div[@data-testid='conversation-turn-{num}']"
         reply_ans = scrape_target(question_inp + "\nI have these datapoints for an image\nGive me some question and answers")
         print(reply_ans)
         data.append({'Description': question_inp, 'Answer': reply_ans})
         num = num+2
+        sucess = sucess + 1
+        qa_data_df = pd.DataFrame(data)
+        data_final_df = pd.merge(data_df, qa_data_df, on='Description', how='inner')
+        data_final_df.to_csv(f"Q&A_epoch_1.csv")
     except:
-        print("error")
+        print("___________________________Error in browser______________________________")
+        qa_data_df = pd.DataFrame(data)
+        data_final_df = pd.merge(data_df, qa_data_df, on='Description', how='inner')
+        data_final_df.to_csv(f"Q&A_{sucess}_error.csv")
         driver.quit()
+        num = 3
         driver = webdriver.Firefox()
         driver.get(url)
         xpath_reply_box = f"//div[@data-testid='conversation-turn-{num}']"
@@ -64,11 +74,19 @@ for question_inp in question_df:
         print(reply_ans)
         data.append({'Description': question_inp, 'Answer': reply_ans})
         num = num+2
+        errors = errors + 1
 
 qa_data_df = pd.DataFrame(data)
-
 data_final_df = pd.merge(data_df, qa_data_df, on='Description', how='inner')
-data_final_df.to_csv("Q&A.csv")
+data_final_df.to_csv("Q&A_final.csv")
+
+print(f" Error : {errors} \nSuccess : {sucess}")
+
+end_time = time.time()
+
+print(f"Start time : {start_time} \nEnd time : {end_time}\nTotal time in mins = {(end_time-start_time)/60}")
+
+print("Scraping finished. Take care")
 driver.quit()
 
 
